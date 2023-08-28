5d01c3b9d772952523126507bc9854b8272e7498
Hannah Morrison <hannah@landhmorrison.scot>
2023-08-28 09:38:45 +0100

    Merge with latest changes

--- keymap.c
+++ keymap.c
@@ -75,10 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [NUM_LAYER] = LAYOUT_split_3x5_2(
-    KC_PAST,  KC_1,    KC_2,     KC_3,    KC_PPLS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_PSLS,  KC_4,    KC_5,     KC_6,    KC_PMNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_DOT,   KC_7,    KC_8,     KC_9,    KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_0,     KC_EQL,  KC_TRNS,  KC_TRNS
+    KC_PAST,  KC_1,     KC_2,     KC_3,    KC_PPLS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_PSLS,  KC_4,     KC_5,     KC_6,    KC_PMNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_DOT,   KC_7,     KC_8,     KC_9,    KC_0,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
   [NAV_LAYER] = LAYOUT_split_3x5_2(
